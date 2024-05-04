3982adada9212d48bc58a0a717aca0062c44b6c6
Nicholas Ang <nicholas.ang@data4life-asia.care>
2024-05-04 16:17:10 +0800

    move folders

---
+++ app/(root)/page.tsx
@@ -0,0 +1,14 @@
+// uses layout defined in ./layout.tsx
+import React from "react";
+import { UserButton } from "@clerk/nextjs";
+
+const Home = () => {
+  return (
+    <div>
+      <p>Home</p>
+      <UserButton afterSignOutUrl="/"/>
+    </div>
+  )
+}
+
+export default Home
--- src/app/(root)/page.tsx
+++
@@ -1,8 +0,0 @@
-// uses layout defined in ./layout.tsx
-import React from "react";
-
-const Home = () => {
-  return <div>Home</div>
-}
-
-export default Home
