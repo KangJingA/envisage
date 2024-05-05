0ef06e5be064b1ebb9c16d02f50e7856baadb8d7
Nicholas Ang <nicholas.ang@data4life-asia.care>
2024-05-05 14:54:23 +0800

    implement sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,10 +2,12 @@
 // ui that is shared between multiple routes
 
 import React from 'react'
+import SideBar from '@/components/shared/SideBar'
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
     return (
         <main className='root'>
+            <SideBar />
             <div className='root-container'>
                 <div className='wrapper'>
                     {children}
